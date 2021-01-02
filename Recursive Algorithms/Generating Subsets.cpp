776f15c557701606e7f1fa4bc8c07456cabdaeb8
Yash Kumar Sahu <CED19I039@iiitdm.ac.in>
2021-01-02 10:09:10 +0530

    Update and rename practice.cpp to Generating Subsets.cpp

--- Recursive Algorithms/Generating Subsets.cpp
+++ Recursive Algorithms/Generating Subsets.cpp
@@ -6,11 +6,11 @@ typedef long long int		ll;
 typedef unsigned long long  ull;
 typedef vector<int> 		vi;
 typedef vector<double> 		vd;
-#define PI 					3.14159265
-#define M					1000000007
-#define M2					998244353
-#define REP(i,a,b) 			for(int i = a; i < b; i++)
-#define PB 					push_back
+#define PI 			3.14159265
+#define M			1000000007
+#define M2			998244353
+#define REP(i,a,b) 		for(int i = a; i < b; i++)
+#define PB 			push_back
 
 int n=3;
 vi subset;
